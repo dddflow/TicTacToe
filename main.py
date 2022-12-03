6b0f147fd4f4d653f0ca4d97589a5360f893cc94
dddflow <d30082002@gmail.com>
2022-12-03 13:39:24 +0000

    add: game space

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print('hello')
+import pygame
+import sys
+from pygame.color import THECOLORS
+
+
+pygame.init()
+
+screen = pygame.display.set_mode((400, 400))
+screen.fill(THECOLORS['azure4'])
+
+pygame.draw.line(screen, THECOLORS['black'], (150, 50), (150, 350), 5)
+pygame.draw.line(screen, THECOLORS['black'], (250, 50), (250, 350), 5)
+pygame.draw.line(screen, THECOLORS['black'], (50, 150), (350, 150), 5)
+pygame.draw.line(screen, THECOLORS['black'], (50, 250), (350, 250), 5)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        pygame.display.flip()
+
+
