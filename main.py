e6627ee90cddf332c65b85d17a0bf699085cb890
dddflow <d30082002@gmail.com>
2022-12-08 17:44:05 +0000

    add: Os & Os game

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ import pygame
 import sys
 from pygame.color import THECOLORS
 
+parts = [pygame.Rect(53, 53, 95, 95), pygame.Rect(153, 53, 95, 95), pygame.Rect(253, 53, 95, 95),
+         pygame.Rect(53, 153, 95, 95), pygame.Rect(153, 153, 95, 95), pygame.Rect(253, 153, 95, 95),
+         pygame.Rect(53, 253, 95, 95), pygame.Rect(153, 253, 95, 95), pygame.Rect(253, 253, 95, 95)]
+areas = [[50, 150, 50, 150], [150, 250, 50, 150], [250, 350, 50, 150],
+         [50, 150, 150, 250], [150, 250, 150, 250], [250, 350, 150, 250],
+         [50, 150, 250, 350], [150, 250, 250, 350], [250, 350, 250, 350]]
+
+Xs = []
+Os = []
+step = 0
+
 pygame.init()
 
 screen = pygame.display.set_mode((400, 400))
@@ -13,33 +24,37 @@ pygame.draw.line(screen, THECOLORS['black'], (250, 50), (250, 350), 5)
 pygame.draw.line(screen, THECOLORS['black'], (50, 150), (350, 150), 5)
 pygame.draw.line(screen, THECOLORS['black'], (50, 250), (350, 250), 5)
 
-parts = [pygame.Rect(53, 53, 95, 95), pygame.Rect(153, 53, 95, 95), pygame.Rect(253, 53, 95, 95),
-         pygame.Rect(53, 153, 95, 95), pygame.Rect(153, 153, 95, 95), pygame.Rect(253, 153, 95, 95),
-         pygame.Rect(53, 253, 95, 95), pygame.Rect(153, 253, 95, 95), pygame.Rect(253, 253, 95, 95)]
-areas = [[50, 150, 50, 150], [150, 250, 50, 150], [250, 350, 50, 150],
-         [50, 150, 150, 250], [150, 250, 150, 250], [250, 350, 150, 250],
-         [50, 150, 250, 350], [150, 250, 250, 350], [250, 350, 250, 350]]
 
-dr = False
+def XO_draw():
+    for x in Xs:
+        pygame.draw.circle(screen, THECOLORS['firebrick1'], x, 40, 4)
+    for o in Os:
+        pygame.draw.circle(screen, THECOLORS['blue'], o, 40, 4)
+
 
 while True:
+    mouse = pygame.mouse.get_pos()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
         elif event.type == pygame.MOUSEBUTTONUP:
-            dr = True
+            for i in range(9):
+                if areas[i][0] < mouse[0] < areas[i][1] and areas[i][2] < mouse[1] < areas[i][3]:
+                    if step % 2 == 0:
+                        Xs.append([(areas[i][0] + areas[i][1]) / 2, (areas[i][2] + areas[i][3]) / 2])
+                    else:
+                        Os.append([(areas[i][0] + areas[i][1]) / 2, (areas[i][2] + areas[i][3]) / 2])
+                    step += 1
+                    XO_draw()
+
         pygame.display.flip()
 
-    mouse = pygame.mouse.get_pos()
     for i in range(9):
         if areas[i][0] < mouse[0] < areas[i][1] and areas[i][2] < mouse[1] < areas[i][3]:
             pygame.draw.rect(screen, THECOLORS['azure3'], parts[i])
         else:
             pygame.draw.rect(screen, THECOLORS['azure4'], parts[i])
-    # if dr:
-    # pygame.draw.circle(screen, THECOLORS['firebrick1'], (100, 100), 40, 4)
 
-
-
-
+    XO_draw()
